e837e675d681990c87f7ac7b5737260390cd7d2f
남연우 <yeonu9464@gmail.com>
2020-06-23 17:43:27 +0900

    산타퀘스트 패널생성

---
+++ Unity_Portfolio/Assets/2.Scripts/SantaQuest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SantaQuest : MonoBehaviour
+{
+    public GameObject santaQuest;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            santaQuest.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            santaQuest.SetActive(false);
+        }
+    }
+}
